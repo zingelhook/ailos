f312e1657bcbb3772e83fa6b79237fb0417e799b
Kevin Gaddy <kevin.gaddy@gmail.com>
2015-01-31 17:38:58 -0600

    init

---
+++ ailos/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ailos
+//
+//  Created by kgaddy on 1/31/15.
+//  Copyright (c) 2015 com.zingelhook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
